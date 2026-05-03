de6151ef9ced7fe5fd5a7ea948fc36c09239d782
Velvetvibes69 <amayasunil11@gmail.com>
2026-05-03 12:51:41 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#Creating different variables
+y = "7/5/2013"
+x = "13/8/2013"
+a = "4/9/2013"
+b = "29/4/1997"
+q = "20/1/1986"
+
+print(13/8/2013)
+print(7/5/2013)
+print(4/9/2013)
+print(29/4/1997)
+print(20/1/1986)
+
+Birthdate = "Birthdate"
+print(Birthdate)
+
+#User Input
+Birthdate= input ("Enter a birth date: ")
+
+print("/nHELLO, This is the birthday you have entered")
